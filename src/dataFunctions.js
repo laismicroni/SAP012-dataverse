7a7590644580d3ccba8d24a896def116011aa4fd
laismicroni <laismicroni@gmail.com>
2024-01-23 20:19:45 -0300

    Ajustes nas funções pós testes

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,3 @@
-export function moviesFiltered(dataset, movieGender) {
-  if(!movieGender){
-    return dataset;
-  }
-}
-
 export const filterData = (data, filterBy, value) => {
   const result = data.filter((item) => item.facts[filterBy].toLowerCase().includes(value));
   return result;
@@ -11,28 +5,17 @@ export const filterData = (data, filterBy, value) => {
 
 export const sortData = (data, sortBy, sortOrder) => {
 
-  const orderMultiplier = sortOrder === 'asc' ? -1 : 1;
+  const orderMultiplier = sortOrder === 'asc' ? 1 : -1;
 
-  data.sort((a, b) => {
+  return data.slice().sort((a, b) => {
     const valueA = a.facts[sortBy];
     const valueB = b.facts[sortBy];
 
-    const numericValueA = parseFloat(valueA);
-    const numericValueB = parseFloat(valueB);
+    const numA = parseFloat(valueA);
+    const numB = parseFloat(valueB);
 
-    if (!isNaN(numericValueA) && !isNaN(numericValueB)) {
-      return (numericValueA - numericValueB) * orderMultiplier;
-    }
-    if (!isNaN(numericValueA) && isNaN(numericValueB)) {
-      return -1 * orderMultiplier;
-    }
-    if (numericValueA > numericValueB) {
-      return 1 * orderMultiplier;
-    }
-    return 0;
+    return (numA - numB) * orderMultiplier;
   });
-
-  return data;
 };
 
 
